58b8d007e24a7ad6e8c2bec9824788b05c24fd3c
hojji <101567172+hojiii@users.noreply.github.com>
2023-02-16 11:19:28 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 101.83 ms, Memory: 56.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,11 @@
+function solution(participant, completion) {
+    let answer = '';
+    let name =participant.sort()
+    let fin =completion.sort()
+    for(let i=0;i<participant.length;i++){
+        if(name[i]!=fin[i]){
+            answer = name[i]
+              return answer; 
+        }
+    }
+}
